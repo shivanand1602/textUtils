c4e02ef4c14841abc8012974424bd7458d68e26b
shivanand1602 <shivanandp187@gmail.com>
2023-02-20 17:05:36 +0530

    Updates

---
+++ precache-manifest.d08bab97a51bf8e0ba9a80c8051b73c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f07eb2a70b6171938662",
+    "url": "/textutils/static/css/main.b48fd515.chunk.css"
+  },
+  {
+    "revision": "f07eb2a70b6171938662",
+    "url": "/textutils/static/js/main.f07eb2a7.chunk.js"
+  },
+  {
+    "revision": "7fc5741dfb5d402e95fa",
+    "url": "/textutils/static/js/1.7fc5741d.chunk.js"
+  },
+  {
+    "revision": "3a93d6f9694f14604a69",
+    "url": "/textutils/static/js/2.3a93d6f9.chunk.js"
+  },
+  {
+    "revision": "4abb4b825711f064af38",
+    "url": "/textutils/static/js/runtime~main.4abb4b82.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/textutils/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "07efc659b3fc2d0f1868366da5d70b9e",
+    "url": "/textutils/index.html"
+  }
+];
